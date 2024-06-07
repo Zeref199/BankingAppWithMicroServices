7fcf1cd46a9b8d15d6343c20222d97228831190a
Otmaneox <arsalane.otmane@gmail.com>
2024-06-07 14:46:34 +0100

    Update audit columns using Spring Data

--- src/main/java/com/easybytes/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/easybytes/accounts/service/impl/AccountServiceImpl.java
@@ -15,7 +15,6 @@ import com.easybytes.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -31,8 +30,6 @@ public class AccountServiceImpl implements IAccountService {
         if(optionalCustomer.isPresent()){
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "+ customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -49,8 +46,6 @@ public class AccountServiceImpl implements IAccountService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
