09a1a829b2fe83c73187c06cd19dfb4e3d7c2e04
Otmaneox <arsalane.otmane@gmail.com>
2024-06-07 14:24:04 +0100

    Perform input validations inside account microservice

--- src/main/java/com/easybytes/accounts/dto/AccountsDto.java
+++ src/main/java/com/easybytes/accounts/dto/AccountsDto.java
@@ -1,10 +1,16 @@
 package com.easybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+    @NotEmpty(message = "Account Number can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
+    @NotEmpty(message = "Account Type can not be null or empty")
     private String accountType;
+    @NotEmpty(message = "Branch Address can not be null or empty")
     private String branchAddress;
 }
